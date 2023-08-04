72ebec12a935c1c2245169102e752bdb33c4b712
John Fox <jfox@mcmaster.ca>
2023-08-04 11:35:29 -0400

    more tests for cvSelect()

--- tests/testthat/test-cvSelect.R
+++ tests/testthat/test-cvSelect.R
@@ -1,10 +1,10 @@
 # test that different algorithms produce the same results
-#  using selectStepAIC()
+#  using selectStepAIC() for a lm
 
 data(Auto, package="ISLR")
 m <- lm(mpg ~ . - name - origin, data=Auto)
 
-test_that("cvSelect naive vs Woodbury", {
+test_that("cvSelect naive vs Woodbury lm", {
   expect_equal(cvSelect(selectStepAIC, Auto, k=5, seed=123,
                         model=m, method="naive"),
                cvSelect(selectStepAIC, Auto, k=5, seed=123,
@@ -13,11 +13,59 @@ test_that("cvSelect naive vs Woodbury", {
 })
 
 # test that parallel computations work correctly using selectStepAIC()
+#  for a lm
 
-test_that("cvSelect naive vs Woodbury", {
+test_that("cvSelect parallel lm", {
   expect_equal(cvSelect(selectStepAIC, Auto, k=5, seed=123,
                         model=m),
                cvSelect(selectStepAIC, Auto, k=5, seed=123,
                         model=m, parallel=TRUE, ncores=2)
                )
 })
+
+# test that different algorithms produce the same results
+#  using selectStepAIC() for a Gaussian glm
+
+m.glm <- lm(mpg ~ . - name - origin, data=Auto)
+
+test_that("cvSelect exact vs Woodbury glm", {
+  expect_equal(cvSelect(selectStepAIC, Auto, k=5, seed=123,
+                        model=m.glm, method="exact"),
+               cvSelect(selectStepAIC, Auto, k=5, seed=123,
+                        model=m.glm, method="Woodbury")
+  )
+})
+
+test_that("cvSelect exact vs hatvalues loo glm", {
+  expect_equal(cvSelect(selectStepAIC, Auto, k="loo",
+                        model=m.glm, method="exact"),
+               cvSelect(selectStepAIC, Auto, k="loo",
+                        model=m.glm, method="hatvalues")
+  )
+})
+
+test_that("cvSelect Woodbury vs hatvalues loo glm", {
+  expect_equal(cvSelect(selectStepAIC, Auto, k="loo",
+                        model=m.glm, method="Woodbury"),
+               cvSelect(selectStepAIC, Auto, k="loo",
+                        model=m.glm, method="hatvalues")
+  )
+})
+
+## FIXME!!! The following works when run directly but fails
+## when run in test suite ???
+
+# test that parallel computations work correctly using selectStepAIC()
+#  for a glm
+
+# data("Caravan", package="ISLR")
+# Caravan <- Caravan[1:500, c(1:10, 86)]
+# m.caravan <- glm(Purchase ~ ., data=Caravan, family=binomial)
+#
+# test_that("cvSelect parallel glm", {
+#   expect_equal(cvSelect(selectStepAIC, Caravan, k=5, seed=123,
+#          model=m.caravan),
+#          cvSelect(selectStepAIC, Caravan, k=5, seed=123,
+#          model=m.caravan, parallel=TRUE, ncores=2)
+#   )
+# })
