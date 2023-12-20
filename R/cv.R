7ca3880b416906a0d5f9f69318cf42fc42569f3e
John Fox <jfox@mcmaster.ca>
2023-12-20 17:19:15 -0500

    another attempt improve getLossFn()

--- R/cv.R
+++ R/cv.R
@@ -634,11 +634,10 @@ getLossFn <- function(criterion){
   which <- grepl("result <- mean\\(", expressions)
   if (!(any(which))) stop(deparse(substitute(criterion)),
                           " does not set 'result <- mean(. . .)'")
-  expression <- sub("result <- mean\\(", "result <- ",
-                    expressions[which][sum(which)])
-  close.parens <- gregexpr(")", expression)[[1]]
-  expression <- substr(expression, 1,
-                       close.parens[length(close.parens)] - 1)
+  expression <- sub("\\)[:space:]*$", "",
+                    sub("result <- mean\\(", "result <- ",
+                        expressions[which][sum(which)])
+  )
   expressions[which] <- expression
   which <- grepl("attr\\(", expressions)
   expressions <- expressions[!which]
