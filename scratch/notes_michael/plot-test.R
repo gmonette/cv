c2590bf731cc7c584a6bec0ba073db98e3985158
Michael Friendly <friendly@yorku.ca>
2024-03-16 18:23:22 -0400

    add error bars to mse plot

--- scratch/notes_michael/plot-test.R
+++ scratch/notes_michael/plot-test.R
@@ -15,5 +15,7 @@ cv.auto.df |>
   mutate(Fold = ifelse(fold==0, "Overall", "Folds")) |>
 
   ggplot(aes(x=mse, y=Fold, color=Fold, shape=Fold)) +
-    geom_point(size = 3)
+    geom_point(size = 3) +
+    geom_errorbarh(aes(xmin = adj.mse.lower, xmax = adj.mse.upper), height=.1)
 
+# but seems I should also show the adjusted.mse and full.mse on the Overall line ????
